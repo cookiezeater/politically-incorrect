5f0efe5a64eb6f57ef41d2f577cc91d5299f1296
Charles <dolphinsandfriends@gmail.com>
2014-06-17 17:17:01 -0700

    added requirements.txt

--- views.py
+++ views.py
@@ -7,5 +7,11 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/cards/")
+def cards():
+    card = Card("Test card", 0, True, "")
+    print card
+    return card
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
