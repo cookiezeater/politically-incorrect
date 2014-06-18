026b72229200d518b3556207df81330912b5df53
Charles <dolphinsandfriends@gmail.com>
2014-06-17 18:18:44 -0700

    stupid simple urls for adding cards

--- views.py
+++ views.py
@@ -1,16 +1,32 @@
 from app import app, db
 from models import *
-from flask import jsonify
+from flask import jsonify, request
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/cards/")
-def cards():
+@app.route("/cards/", methods=["GET"])
+def get_all_cards():
     cards = Card.query.all()
-    cards = {card.text: "white" if card.white else "black" for card in cards}
+    cards = {card.text: card.id for card in cards}
     return jsonify(**cards)
+
+@app.route("/cards/<int:card_id>", methods=["GET"])
+def get_card(card_id):
+    card = Card.query.get(card_id)
+    return jsonify(card=card.text)
+
+@app.route("/cards/", methods=["POST"])
+def create_card():
+    content = request.json
+
+    card = Card(text=content["text"],
+                rank=0,
+                white=bool(content["white"]))
+    db.session.add(card)
+    db.session.commit()
+    return jsonify(status="success")
 
 if __name__ == "__main__":
     db.create_all()
