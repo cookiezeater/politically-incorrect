34a4d5a23f97850a25585aded330fe3d36216a3f
Charles <cshao4@illinois.edu>
2014-06-19 11:54:32 -0700

    fix relationships, removed unnecessary functs

--- models.py
+++ models.py
@@ -8,10 +8,25 @@
 
 from app import db
 
-# association_table = db.Table('association', db.Model.metadata,
-#     db.Column('left_id', db.Integer, db.ForeignKey('left.id')),
-#     db.Column('right_id', db.Integer, db.ForeignKey('right.id'))
-# )
+players_to_matches = db.Table('p2m', db.Model.metadata,
+    db.Column('players_id', db.Integer, db.ForeignKey('players.id')),
+    db.Column('matches_id', db.Integer, db.ForeignKey('matches.id')))
+
+players_to_players = db.Table('p2p', db.Model.metadata,
+    db.Column('players_id', db.Integer, db.ForeignKey('players.id')),
+    db.Column('players_id', db.Integer, db.ForeignKey('players.id')))
+
+states_to_cards = db.Table('s2c', db.Model.metadata,
+    db.Column('states_id', db.Integer, db.ForeignKey('states.id')),
+    db.Column('cards_id', db.Integer, db.ForeignKey('cards.id')))
+
+matches_to_players = db.Table('m2p', db.Model.metadata,
+    db.Column('matches_id', db.Integer, db.ForeignKey('matches.id')),
+    db.Column('players_id', db.Integer, db.ForeignKey('players.id')))
+
+matches_to_cards = db.Table('m2c', db.Model.metadata,
+    db.Column('matches_id', db.Integer, db.ForeignKey('matches.id')),
+    db.Column('cards_id', db.Integer, db.ForeignKey('cards.id')))
 
 class Card(db.Model):
     """
@@ -20,12 +35,17 @@ class Card(db.Model):
     """
 
     __tablename__ = 'cards'
-
     id = db.Column(db.Integer, primary_key=True)
+
     text = db.Column(db.String(120), unique=True)
     rank = db.Column(db.Integer)
     white = db.Column(db.Boolean)
     meta = db.Column(db.String(500))
+
+    # One-to-many:
+    # A match has one black card at any time,
+    # but a black card can be in many matches.
+    matches = db.relationship('Match', backref='cards')
 
     def __init__(self, text=None, rank=0, white=True, meta=""):
         self.text = text
@@ -33,123 +53,138 @@ class Card(db.Model):
         self.white = white
         self.meta = meta
 
-    def __repr__(self):
-        return self.__str__()
+    def __str__(self):
+        return "{} Card: {}".format("White" if self.white else "Black",
+                                    self.text)
+
+class Player(db.Model):
+    """
+    Represents a player.
+    """
+
+    __tablename__ = 'players'
+    id = db.Column(db.Integer, primary_key=True)
+
+    username = db.Column(db.String(50), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    losses = db.Column(db.Integer)
+
+    # One-to-many:
+    # A match only has one winner, but a winner can have many won matches.
+    wins = db.relationship('Match', backref='players')
+
+    # Many-to-many:
+    # A player has many friends,
+    # and any friend can have many friends
+    friends = db.relationship("Player", secondary=players_to_players,
+                                backref="players")
+
+    # Many-to-many:
+    # A player can be in many matches,
+    # and matches have many players
+    matches = db.relationship("Match", secondary=players_to_matches,
+                                backref="players")
+
+    # One-to-many:
+    # A player has many states, but a state can only have one player.
+    states = db.relationship('State', backref='players')
+
+    def __init__(self, username=None, email=None,
+                    first_name="", last_name=""):
+        self.username = username
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+
+        self.matches = None
+        self.friends = None
+        self.wins = 0
+        self.losses = 0
 
     def __str__(self):
-        return "{} Card: {}".format("White" if self.white else "Black", self.text)
+        return "Player {}: {}".format(self.username, self.email)
 
-# class Player(db.Model):
-#     """
-#     Represents a player.
-#     """
+class Match(db.Model):
+    """
+    @todo:
+        - add table-level enforcement of status choices
+    """
 
-#     __tablename__ = 'players'
+    STATUSES = "PENDING", "ONGOING", "ENDED"
 
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(50), unique=True)
-#     email = db.Column(db.String(120), unique=True)
-#     first_name = db.Column(db.String(50))
-#     last_name = db.Column(db.String(50))
-#     matches = db.relationship("Match",
-#                     secondary=association_table) # Many-to-many db.relationship
-#     friends = db.relationship("Player",
-#                     secondary=association_table) # Many-to-many db.relationship
-#     wins = db.Column(db.Integer)
-#     losses = db.Column(db.Integer)
+    __tablename__ = 'matches'
+    id = db.Column(db.Integer, primary_key=True)
 
-#     def __init__(self, username=None, email=None,
-#                     first_name="", last_name=""):
-#         self.username = username
-#         self.email = email
-#         self.first_name = first_name
-#         self.last_name = last_name
+    status = db.Column(db.String(7))
 
-#         self.matches = None
-#         self.friends = None
-#         self.wins = 0
-#         self.losses = 0
+    # One-to-many:
+    # A match has many states, but a state can only have one match.
+    states = db.relationship('State', backref='match')
 
-#     def __str__(self):
-#         return "Player {}: {}".format(self.username, self.email)
+    # Many-to-many:
+    # A match can have many pending players,
+    # and a pending player can have many pending matches.
+    pending = db.relationship("Player", secondary=matches_to_players)
 
-#     def friends(self):
-#         raise NotImplementedError
+    # Many-to-one:
+    # A match only has one winner, but a winner can have many won matches.
+    winner_id = db.Column(db.Integer, db.ForeignKey('players.id'))
 
-#     def ended_matches(self):
-#         raise NotImplementedError
+    # Many-to-many:
+    # A match has many cards in a deck,
+    # and cards can have many decks in many matches.
+    deck = db.relationship("Card", secondary=matches_to_cards)
 
-#     def ongoing_matches(self):
-#         raise NotImplementedError
+    # Many-to-one:
+    # A match has one black card at any time,
+    # but a black card can be in many matches.
+    black_id = db.Column(db.Integer, db.ForeignKey('cards.id'))
 
-#     def all_matches(self):
-#         raise NotImplementedError
+    def __init__(self,
+                host=None,
+                status="PENDING",
+                state=None,
+                pending=[],
+                table=None,
+                winner_id=-1,
+                winner=None):
+        raise NotImplementedError
 
-# class Match(db.Model):
-#     """
-#     @todo:
-#         - add table-level enforcement of status choices
-#     """
+    def __str__(self):
+        players = "get_all_players"
+        raise NotImplementedError
+        return "Match players: {}, status: {}, judge: {}".format(players, self.status, self.judge)
 
-#     STATUSES = "PENDING", "ONGOING", "ENDED"
+class State(db.Model):
+    __tablename__ = 'states'
+    id = db.Column(db.Integer, primary_key=True)
 
-#     __tablename__ = 'matches'
+    score = db.Column(db.Integer)
+    judged = db.Column(db.Integer)
 
-#     id = db.Column(db.Integer, primary_key=True)
-#     status = db.Column(db.String(7))
-#     state = db.relationship("State", db.backref="matches") # One-to-many db.relationship
-#     pending = db.relationship("Player",
-#                     secondary=association_table) # Many-to-many db.relationship
-#     table = db.relationship("db.Table",
-#                     uselist=False, db.backref="matches") # One-to-one db.relationship
+    # Many-to-one:
+    # A state has one player, but a player can have many states.
+    player_id = db.Column(db.Integer, db.ForeignKey('players.id'))
 
-#     # Many-to-one db.relationship
-#     winner_id = db.Column(db.Integer, db.ForeignKey('player.id'))
-#     winner = db.relationship("Player")
+    # Many-to-one:
+    # A state has one match, but a match has many states.
+    match_id = db.Column(db.Integer, db.ForeignKey('matches.id'))
 
-#     def __init__(self, host=None, status="PENDING",
-#                     state=None, pending=[], table=None,
-#                     winner_id=-1, winner=None):
-#         raise NotImplementedError
+    # Many-to-many:
+    # A state has a hand with many cards,
+    # and cards can belong to many hands in many states.
+    hand = db.relationship("Card", secondary=states_to_cards)
 
-#     def __str__(self):
-#         players = "get_all_players"
-#         raise NotImplementedError
-#         return "Match players: {}, status: {}, judge: {}".format(players, self.status, self.judge)
+    # Many-to-one:
+    # A state has one played card,
+    # but a card can have many states in which it is played.
+    played = db.relationship("Card", backref="states")
 
-#     def status(self):
-#         raise NotImplementedError
+    def __init__(self, player=None, match=None, score=0,
+                    hand=[], played=None, judged=0):
+        raise NotImplementedError
 
-# class db.Table(db.Model):
-#     __tablename__ = 'tables'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     deck = db.relationship("Card",
-#                 secondary=association_table) # Many-to-many db.relationship
-#     black = db.relationship("Card", db.backref="tables") # Many-to-one db.relationship
-
-#     def __init__(self, deck=[], black=None):
-#         raise NotImplementedError
-
-#     def __str__(self):
-#         return "db.Table"
-
-# class State(db.Model):
-#     __tablename__ = 'states'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     player = db.relationship("Player", db.backref="states") # Many-to-one db.relationship
-#     match = db.relationship("Match",
-#                     uselist=False, db.backref="states") # One-to-one db.relationship
-#     score = db.Column(db.Integer)
-#     hand = db.relationship("Card",
-#                     secondary=association_table) # Many-to-many db.relationship
-#     played = db.relationship("Card", db.backref="states") # Many-to-one db.relationship
-#     judged = db.Column(db.Integer)
-
-#     def __init__(self, player=None, match=None, score=0,
-#                     hand=[], played=None, judged=0):
-#         raise NotImplementedError
-
-#     def __str__(self):
-#         return "State: Player {}, Match {}".format(self.player, self.match)
+    def __str__(self):
+        return "State: Player {}, Match {}".format(self.player, self.match)
