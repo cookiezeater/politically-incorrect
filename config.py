fb29b92d46e4008a59978de2f2a5372bd6537122
Charles <dolphinsandfriends@gmail.com>
2014-06-23 21:13:08 -0700

    pep8

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
+
 class Config(object):
     DEBUG = False
     TESTING = False
@@ -7,16 +8,20 @@ class Config(object):
     SECRET_KEY = 'this-really-needs-to-be-changed'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
+
 class ProductionConfig(Config):
     DEBUG = False
+
 
 class StagingConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
 
+
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
+
 
 class TestingConfig(Config):
     TESTING = True
