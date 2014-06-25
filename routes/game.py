28f74cd3b284d1319ed91ce44f302e548d326415
Charles <dolphinsandfriends@gmail.com>
2014-06-24 22:05:04 -0700

    implement match invite and accept

--- routes/game.py
+++ routes/game.py
@@ -1,11 +1,37 @@
 from routes.shared import *
 
-@app.route("/players/<int:player_id>", methods=["POST"])
-def invite_player(player_id):
+
+@app.route("/matches/<int:match_id>/invite", methods=["POST"])
+def invite_player(match_id):
     content = request.json
-    match = Match.query.get(content["match_id"])
-    assert match.host_id != player_id
-    match.pending.append(Player.query.get(player_id))
+    match = Match.query.get(match_id)
+    assert match.host_id == content["inviter"]
+    match.pending.append(Player.query.get(content["invitee"]))
+    db.session.add(match)
+    db.session.commit()
+    return jsonify(status="success")
+
+
+@app.route("/matches/<int:match_id>/accept", methods=["POST"])
+def accept_invite(match_id):
+    """
+    Accept an invite. First, assert that the acceptor is not
+    the host of the match. Second, assert that the match
+    is in the acceptor's list of invites. Then, remove
+    the match from the list. Finally, create a state that
+    links the match to the accepting player.
+    """
+
+    content = request.json
+    match = Match.query.get(match_id)
+    assert match.host_id != content["acceptor_id"]
+    acceptor = Player.query.get(content["acceptor_id"])
+    print acceptor.invited
+    assert match in acceptor.invited
+    acceptor.invited.remove(match)
+    state = State(acceptor.id, match.id)
+    db.session.add(state)
+    match.states.append(state)
     db.session.add(match)
     db.session.commit()
     return jsonify(status="success")
