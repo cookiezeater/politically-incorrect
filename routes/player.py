a33777a23550f45cd3822e5584834b7e469ea57a
Quinn Jarrell <qjarrell@gosynapsify.com>
2014-06-25 22:55:25 -0400

    added checks to see if a player exists. TODO make this check into a decorator

--- routes/player.py
+++ routes/player.py
@@ -4,49 +4,53 @@ from routes.shared import *
 @app.route("/players", methods=["GET"])
 def get_all_players():
     players = Player.query.all()
-    players = {player.email: player.id
-               for player in players}
+    players = {player.email: [player.id
+               for player in players]}
     return jsonify(**players)
 
 @app.route("/players/<int:player_id>", methods=["GET"])
 def get_player(player_id):
     player = Player.query.get(player_id)
-    return jsonify(username=player.username,
-                   email=player.email,
-                   first_name=player.first_name,
-                   last_name=player.last_name,
-                   invited=[invitation.id for invitation in player.invited])
+    if player:
+        player = Player.query.get(player_id)
+        return jsonify(username=player.username,
+                       email=player.email,
+                       first_name=player.first_name,
+                       last_name=player.last_name,
+                       )
+    else:
+        return jsonify(status="failure: player does not exist.")
 
 @app.route("/players/<int:player_id>", methods=["PUT"])
 def update_player(player_id):
     player = Player.query.get(player_id)
-    content = request.json
-    if "username" in content:
-        player.username = content[username]
-    if "password" in content:
-        player.password = content[password]
-    if "email" in content:
-        player.email = content[email]
-    if "first_name" in content:
-        player.first_name = content[first_name]
-    if "last_name" in content:
-        player.last_name = content[last_name]
-    db.session.add(player)
-    db.session.commit()
-    return jsonify(status="success")
-
+    if player:
+        content = request.json
+        if "username" in content:
+            player.username = content[username]
+        if "password" in content:
+            player.password = content[password]
+        if "email" in content:
+            player.email = content[email]
+        if "first_name" in content:
+            player.first_name = content[first_name]
+        if "last_name" in content:
+            player.last_name = content[last_name]
+        db.session.add(player)
+        db.session.commit()
+        return jsonify(status="success")
+    else:
+        return jsonify(status="failure: player does not exist.")
 @app.route("/players/<int:player_id>", methods=["DELETE"])
 def delete_player(player_id):
     """Deleting a player cascades to all of its relationships."""
-
-    player = Card.query.get(player_id)
+    player = Player.query.get(player_id)
     if player:
         db.session.delete(player)
         db.session.commit()
         return jsonify(status="success")
     else:
         return jsonify(status="failure: player does not exist.")
-
 @app.route("/players", methods=["POST"])
 def create_player():
     content = request.json
