9ccde2895f91ce704de95c42d7eb356127e87976
Charles <dolphinsandfriends@gmail.com>
2014-07-01 22:41:48 -0700

    runnable from any dir

--- scaffolds/card_scaffold.py
+++ scaffolds/card_scaffold.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python
 """Initiates database with some cards."""
+import os
 import json
 import requests
 
 
 if __name__ == "__main__":
+    white_cards_file = os.path.join(os.path.dirname(__file__),
+                       "../cards/white.txt")
+    black_cards_file = os.path.join(os.path.dirname(__file__),
+                       "../cards/black.txt")
     headers = {"Content-type": "application/json",
                "Accept": "application/json"}
     location = "http://localhost:5000/cards"
 
-    with open("../cards/white.txt", "r") as white_cards, \
-         open("../cards/black.txt", "r") as black_cards:
+    with open(white_cards_file, "r") as white_cards, \
+         open(black_cards_file, "r") as black_cards:
         for card_text in white_cards:
             requests.post(location,
                           data=json.dumps({"text": card_text.strip(),
-                                           "white": True}),
+                                           "white": True,
+                                           "answers": 0}),
                           headers=headers)
         for card_text in black_cards:
+            answers = card_text.count("_")
             requests.post(location,
                           data=json.dumps({"text": card_text.strip(),
-                                           "white": False}),
+                                           "white": False,
+                                           "answers": answers
+                                                      if answers
+                                                      else 1}),
                           headers=headers)
