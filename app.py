a87d5b74493a4e826cecbbfa9189ee027de438e7
Charles <cshao4@illinois.edu>
2014-07-30 13:40:36 -0700

    add app meta info

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ from routes.match import *
 from routes.game import *
 from common import app, db
 
+
+__author__ = "Charles S."
+__repository__ = "https://github.com/25cf/friends-against-humanity"
+__status__ = "Development"
+
+
 if __name__ == "__main__":
     db.create_all()
     app.run()
