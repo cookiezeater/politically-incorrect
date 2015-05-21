76100ad0d4c8e81be0381baff05896b4b76bfc48
25cf <dolphinsandfriends@gmail.com>
2015-05-21 02:44:02 -0500

    stubbing test cases

--- test/test_user.py
+++ test/test_user.py
@@ -1,10 +1,8 @@
 import sys
 import json
-from app import app, db
 from nose.tools import with_setup
-from unittest import (
-    TestCase
-)
+from unittest import TestCase
+from app import app, db
 
 
 class TestUser(TestCase):
@@ -14,7 +12,7 @@ class TestUser(TestCase):
     def tearDown(self):
         pass
 
-    def test_registration(self):
+    def test_new_registration(self):
         oauth_token = 'ya29.egEj1mjXGkAtbSnBuRXC7oRJMxaLfs3rQ_y8B-56akWHoP7TdAK3wJbaAG4S0weYWdmz28XbBi46mg'
         response = self.app.post(
             '/user', data=json.dumps({ 'token': oauth_token }), headers={ 'content-type': 'application/json' }
@@ -23,3 +21,9 @@ class TestUser(TestCase):
 
         assert content['email'] == 'dolphinsandfriends@gmail.com'
         assert content['token'] != oauth_token
+
+    def test_existing_registration(self):
+        pass
+
+    def test_login(self):
+        pass
