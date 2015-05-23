1e62ba1dcdb5b4ec1fa80e28b8f3adb1545b594e
25cf <dolphinsandfriends@gmail.com>
2015-05-22 21:13:31 -0700

    update db config

--- config.py
+++ config.py
@@ -6,8 +6,11 @@
     in common.py.
 """
 
+import os
+
 class Base(object):
     SECRET_KEY = '123456789lol'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgres://localhost/politically-incorrect-db')
 
 
 class Production(Base):
@@ -20,7 +23,6 @@ class Development(Base):
     DEBUG    = True
     DATABASE = 'politically-incorrect-db'
     USERNAME = 'Charles'
-    SQLALCHEMY_DATABASE_URI = 'postgres://localhost/politically-incorrect-db'
 
     PROPAGATE_EXCEPTIONS        = True
     JSONIFY_PRETTYPRINT_REGULAR = True
